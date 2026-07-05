3f892f2539b327eb297734fd33d14d168d0ecef1
jiawadhwa6 <164456603+jiawadhwa6@users.noreply.github.com>
2026-07-05 18:59:17 +0100

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return
+        
+        # Step 1: Find the middle of the list using slow/fast pointers
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        # Step 2: Reverse the second half of the list
+        prev, curr = None, slow.next
+        slow.next = None  # Cut the first half from the second half
+        
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            
+        # Step 3: Merge the two halves (head and prev)
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            
+            first.next = second
+            second.next = tmp1
+            
+            first = tmp1
+            second = tmp2
